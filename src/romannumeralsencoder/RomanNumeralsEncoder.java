0c8c13ff4219d5861a523aab437a9c4ece23d98e
jesus <jesus@DESKTOP-K48KI8R>
2017-09-16 23:43:42 +0200

    intento 2 revisar 91

--- src/romannumeralsencoder/RomanNumeralsEncoder.java
+++ src/romannumeralsencoder/RomanNumeralsEncoder.java
@@ -16,8 +16,9 @@ public class RomanNumeralsEncoder {
      */
     public static void main(String[] args) {
         String sol = "";
-        int n = 14;
+        int n = 19;
         int contador = 0;
+        int auxiliar=0;
         while ((n - 1000) > 0) {
             n = n - 1000;
 
@@ -33,16 +34,13 @@ public class RomanNumeralsEncoder {
             contador = contador + 1;
 
         }
-        if (n - 1 == 0) {
-                            n = n - 1;
-                            sol = sol + "I";
-                        }
-        if (n - 500 == 0) {
+
+        if (n - 500 == 0 && contador < 3) {
             n = n - 500;
             sol = sol + "D";
         }
         if (contador > 3) {
-            sol = sol + "DM";
+            sol = sol + "CM";
         } else {
             for (int x = 0; x < contador; x++) {
                 sol = sol + "D";
@@ -53,7 +51,7 @@ public class RomanNumeralsEncoder {
             n = n - 100;
             contador = contador + 1;
         }
-        if (n - 100 == 0) {
+        if (n - 100 == 0 && contador < 3) {
             n = n - 100;
             sol = sol + "C";
         }
@@ -69,12 +67,12 @@ public class RomanNumeralsEncoder {
                 contador = contador + 1;
 
             }
-            if (n - 50 == 0) {
+            if (n - 50 == 0 && contador < 3) {
                 n = n - 50;
                 sol = sol + "L";
             }
             if (contador > 3) {
-                sol = sol + "LC";
+                sol = sol + "XC";
             } else {
                 for (int x = 0; x < contador; x++) {
                     sol = sol + "L";
@@ -85,7 +83,7 @@ public class RomanNumeralsEncoder {
                     contador = contador + 1;
 
                 }
-                if (n - 10 == 0) {
+                if (n - 10 == 0 && contador < 3) {
                     n = n - 10;
                     sol = sol + "X";
                 }
@@ -95,47 +93,51 @@ public class RomanNumeralsEncoder {
                     for (int x = 0; x < contador; x++) {
                         sol = sol + "X";
                     }
-                    contador = 0;
-                    while ((n - 5) > 0) {
-                        n = n - 5;
-                        contador = contador + 1;
+                }
+                contador = 0;
+                while ((n - 5) > 0) {
+                    auxiliar=auxiliar+n;
+                    n = n - 5;
+                    contador = contador + 1;
 
-                    }
-                    if (n - 5 == 0) {
-                        n = n - 5;
+                }
+                if ((n - 5 == 0 && contador < 3) && 5+n!=9) {
+                    
+                    n = n - 5;
+                    sol = sol + "V";
+                }
+                if (contador > 3 || auxiliar+1==10) {
+                    sol = sol + "IX";
+                    n=n-9;
+                } else {
+                    for (int x = 0; x < contador; x++) {
                         sol = sol + "V";
                     }
-                    if (contador > 3) {
-                        sol = sol + "VX";
-                    } else {
-                        for (int x = 0; x < contador; x++) {
-                            sol = sol + "V";
-                        }
-                        contador = 0;
-                        while ((n - 1) > 0) {
-                            n = n - 1;
-                            contador = contador + 1;
-
-                        }
-                        
-                        if (n - 1 == 0) {
-                            n = n - 1;
-                            sol = sol + "I";
-                        }
-                        if (contador > 3) {
-                            sol = sol + "IV";
-                        } else {
-                            for (int x = 0; x < contador; x++) {
-                                sol = sol + "I";
-                            }
-                            contador = 0;
-
-                        }
-
-                    }
                 }
+                contador = 0;
+                while ((n - 1) > 0) {
+                    n = n - 1;
+                    contador = contador + 1;
+
+                }
+
+                if (n - 1 == 0 && contador < 3) {
+                    n = n - 1;
+                    sol = sol + "I";
+                }
+                if (contador > 3 || n == 1) {
+                    sol = sol + "IV";
+                } else {
+                    for (int x = 0; x < contador; x++) {
+                        sol = sol + "I";
+                    }
+                    contador = 0;
+
+                }
+
             }
         }
         System.out.println(sol);
     }
+
 }
