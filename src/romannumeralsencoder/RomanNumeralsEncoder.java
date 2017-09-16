99016f0549e1bcab1b9f49ed30fb7760dd0b819b
jesus <jesus@DESKTOP-K48KI8R>
2017-09-16 23:04:46 +0200

    intento 1

---
+++ src/romannumeralsencoder/RomanNumeralsEncoder.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romannumeralsencoder;
+
+/**
+ *
+ * @author jesus
+ */
+public class RomanNumeralsEncoder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String sol = "";
+        int n = 14;
+        int contador = 0;
+        while ((n - 1000) > 0) {
+            n = n - 1000;
+
+            sol = sol + "M";
+
+        }
+        if (n - 1000 == 0) {
+            n = n - 1000;
+            sol = sol + "M";
+        }
+        while ((n - 500) > 0) {
+            n = n - 500;
+            contador = contador + 1;
+
+        }
+        if (n - 1 == 0) {
+                            n = n - 1;
+                            sol = sol + "I";
+                        }
+        if (n - 500 == 0) {
+            n = n - 500;
+            sol = sol + "D";
+        }
+        if (contador > 3) {
+            sol = sol + "DM";
+        } else {
+            for (int x = 0; x < contador; x++) {
+                sol = sol + "D";
+            }
+            contador = 0;
+        }
+        while ((n - 100) > 0) {
+            n = n - 100;
+            contador = contador + 1;
+        }
+        if (n - 100 == 0) {
+            n = n - 100;
+            sol = sol + "C";
+        }
+        if (contador > 3) {
+            sol = sol + "CD";
+        } else {
+            for (int x = 0; x < contador; x++) {
+                sol = sol + "C";
+            }
+            contador = 0;
+            while ((n - 50) > 0) {
+                n = n - 50;
+                contador = contador + 1;
+
+            }
+            if (n - 50 == 0) {
+                n = n - 50;
+                sol = sol + "L";
+            }
+            if (contador > 3) {
+                sol = sol + "LC";
+            } else {
+                for (int x = 0; x < contador; x++) {
+                    sol = sol + "L";
+                }
+                contador = 0;
+                while ((n - 10) > 0) {
+                    n = n - 10;
+                    contador = contador + 1;
+
+                }
+                if (n - 10 == 0) {
+                    n = n - 10;
+                    sol = sol + "X";
+                }
+                if (contador > 3) {
+                    sol = sol + "XL";
+                } else {
+                    for (int x = 0; x < contador; x++) {
+                        sol = sol + "X";
+                    }
+                    contador = 0;
+                    while ((n - 5) > 0) {
+                        n = n - 5;
+                        contador = contador + 1;
+
+                    }
+                    if (n - 5 == 0) {
+                        n = n - 5;
+                        sol = sol + "V";
+                    }
+                    if (contador > 3) {
+                        sol = sol + "VX";
+                    } else {
+                        for (int x = 0; x < contador; x++) {
+                            sol = sol + "V";
+                        }
+                        contador = 0;
+                        while ((n - 1) > 0) {
+                            n = n - 1;
+                            contador = contador + 1;
+
+                        }
+                        
+                        if (n - 1 == 0) {
+                            n = n - 1;
+                            sol = sol + "I";
+                        }
+                        if (contador > 3) {
+                            sol = sol + "IV";
+                        } else {
+                            for (int x = 0; x < contador; x++) {
+                                sol = sol + "I";
+                            }
+                            contador = 0;
+
+                        }
+
+                    }
+                }
+            }
+        }
+        System.out.println(sol);
+    }
+}
